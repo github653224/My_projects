28fc19ca17ed8fb2ac334894c3b68354063573e0
Xueyan Pan <944851899@qq.com>
2017-08-12 14:40:11 +0800

    modfied kw

--- java_class.java
+++ java_class.java
@@ -2,7 +2,7 @@ public class java_class
 	{
 	public static void main ( String [] args)
 		{
-			System.out.println("Hello World"); #我在远程注释了些东西，您可以pull下代码看哦
+			System.out.println("Hello World"); //我在远程注释了些东西，您可以pull下代码看哦
 		
 		}
 	
